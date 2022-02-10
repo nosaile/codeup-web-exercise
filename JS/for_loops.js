d5f00828e47feab4c41d6b0984fb088403368c8e
Chris Eliason <eliason.chris1@gmail.com>
2022-02-10 14:05:15 -0600

    for loop exercises done

--- JS/for_loops.js
+++ JS/for_loops.js
@@ -1,38 +1,73 @@
 "use strict";
 
-
-
-
-// function showMultiplicationTable(x){
-//     console.log(x + " x " + 1 + " = " + x * 1)
-//     console.log(x + " x " + 2 + " = " +  x * 2)
-//     console.log(x + " x " + 3 + " = " +  x * 3)
-//     console.log(x + " x " + 4 + " = " +  x * 4)
-//     console.log(x + " x " + 5 + " = " +  x * 5)
-//     console.log(x + " x " + 6 + " = " +  x * 6)
-//     console.log(x + " x " + 7 + " = " +  x * 7)
-//     console.log(x + " x " + 8 + " = " +  x * 8)
-//     console.log(x + " x " + 9 + " = " +  x * 9)
-//     console.log(x + " x " + 10 + " = " +  x * 10)
+// TODO: Use a for loop to print the even numbers between 0 and 30, inclusive.
+//  HINT: remember the remainder operator (modulo)
+// var i = 0
+// for(i = 0; i <= 30; i++ ){
+//     if(i % 2 === 1){
+//         console.log("odd")
+//     }else if(i % 2 === 0){
+//         console.log(i)
+//     }
+//
 // }
-// showMultiplicationTable(0)
-//
 
+
+// TODO: loop through numbers 1 - 40. Print "marco" if it's divisible by 3. Print "polo" if it's divisible by 5.
+//  -> If it's neither print "I'm not playing"
+//  -> Print "marco polo" if it's divisible by 3 and 5.
+// var i = 1
 //
+// for(i = 1; i <= 40; i++){
+//     if(i % 3 === 0 && i % 5 === 0 ){
+//         console.log("marco polo " + i)}
+//     else if(i % 3 === 0){
+//         console.log("marco " + i)
+//     }else if(i % 5 === 0){
+//         console.log("polo " + i)
+//     }
+//     else console.log("im not playing " + i)
 //
+// }
+
+
+
+
+function showMultiplicationTable(x){
+    for(i = 1; i <= 10; i++){
+        console.log(x + "x" + i + "=" + (x * i))
+    }
+}
+showMultiplicationTable(10)
+
+
+
+
+
 for (var y = 0; y < 10; y++) {
     var count = 0
-    for (var x = 0; x < Math.floor(Math.random()*(200-20)+20); x++){
+    for (var x = 0; x < Math.floor(Math.random() * (200 - 20) * 20); x++){
         count++
     }
     if(x % 2 === 0){
         console.log(count + " " + "is an even number.")
     }else
         console.log(count + " " + "is an odd number.")
-
 }
 
 
+
+for(var i = 1; i < 10; i++){
+    console.log((i + "").repeat(i))
+}
+
+
+
+for(var i = 100; i > 0; i--){
+    if(i % 5 === 0){
+        console.log(i)
+    }
+}
 
 
 
--- JS/while.js
+++ JS/while.js
@@ -1,9 +1,27 @@
-//
-var x = 1
+ "use strict";
 
-
+var x = 1;
 
 while(x < 65536){
-     x *= 2
-    console.log(x)
+    x *= 2;
+    console.log(x);
 }
+
+ var allCones = Math.floor(Math.random() * 50) + 50;
+
+do{
+    let custCones = Math.floor(Math.random() * 5) + 1;
+    console.log(allCones - custCones);
+
+}while(console.log(10)){
+
+ }
+
+
+
+
+
+
+
+
+
