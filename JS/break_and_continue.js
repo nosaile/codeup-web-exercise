4a85d30fb4ad905867c54c92e24492be6951eeaa
Chris Eliason <eliason.chris1@gmail.com>
2022-02-10 15:24:39 -0600

    commit

---
+++ JS/break_and_continue.js
@@ -0,0 +1,18 @@
+"use strict";
+
+
+
+
+var userInput = prompt("Enter an odd number")
+var oddNum = userInput % 2 === 1
+
+while(oddNum % 2 === 1){
+    if(userInput % 2 === 0){
+        continue
+    }
+
+}
+
+
+
+
---
+++ JS/while_loops_extra.js
@@ -0,0 +1,10 @@
+"use strict";
+
+
+
+
+function add(x, y){
+    return
+
+}
+
