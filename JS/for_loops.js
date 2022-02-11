b33ccf1dd907b761a3a8354f5f41e50cccf22384
Chris Eliason <eliason.chris1@gmail.com>
2022-02-11 09:39:53 -0600

    for_loops done

--- JS/for_loops.js
+++ JS/for_loops.js
@@ -12,7 +12,7 @@ for(i = 0; i <= 30; i++ ){
 
 }
 
-
+//git
 // TODO: loop through numbers 1 - 40. Print "marco" if it's divisible by 3. Print "polo" if it's divisible by 5.
 //  -> If it's neither print "I'm not playing"
 //  -> Print "marco polo" if it's divisible by 3 and 5.
