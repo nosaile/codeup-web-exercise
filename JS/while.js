83f26dc8e49ec915317bda145e9a7d9d0117313f
Chris Eliason <eliason.chris1@gmail.com>
2022-02-17 16:54:19 -0600

    still working on this

--- JS/while.js
+++ JS/while.js
@@ -27,23 +27,26 @@
 //     console.log("You can't sell that much ice cream because you don't have enough in stock, find a new customer.");
 // }
 // }
- var allCones = Math.floor(Math.random() * 50) + 50;
- var cones = allCones;
 
- do{
+
+ var cones = Math.floor(Math.random() * 50) + 50;
+ do {
      var custCones = Math.floor(Math.random() * 5) + 1;
-     cones;
-     cones - custCones;
+     var afterSale = cones-=custCones
      console.log(cones + " is how many cones we need to sell.");
-     console.log(custCones + " is how many they want to buy right now.");
-     console.log(cones - custCones + " is how many are left now.");
-     cones -= custCones;
+     console.log(afterSale + " is how many cones are left now.");
+     console.log(custCones + " is how many cones they want to buy right now.");
+     afterSale;
+    }
+ while(afterSale - custCones >= 0) {
 
- }while(cones - custCones >= 0){
-     if(cones < 0){
-         console.log("You can't sell that much ice cream because you don't have enough in stock, find a new customer.");
-     }
+
+
  }
+
+
+
+
 
 //
 
