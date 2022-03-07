09d5e08df25ea5aff01598fc0bdfa4b9570e2da1
Chris Eliason <eliason.chris1@gmail.com>
2022-03-07 15:14:08 -0600

    ice cream do while loop completed

--- JS/while.js
+++ JS/while.js
@@ -1,4 +1,4 @@
- "use strict";
+"use strict";
 
 // var x = 1;
 //
@@ -8,44 +8,35 @@
 // }
 
 
+var allCones = Math.floor(Math.random() * 50) + 50;
+var cones = allCones;
 
- //this one almost works
-//  var allCones = Math.floor(Math.random() * 50) + 50;
-//  var cones = allCones;
-//
-// do{
-//     var custCones = Math.floor(Math.random() * 5) + 1;
-//     cones;
-//     cones - custCones;
-//     console.log(cones + " is how many cones we need to sell.");
-//     console.log(custCones + " is how many they want to buy right now.");
-//     console.log(cones - custCones + " is how many are left now.");
-//     cones -= custCones;
-//
-// }while(cones - custCones >= 0){
-// if(cones < 0){
-//     console.log("You can't sell that much ice cream because you don't have enough in stock, find a new customer.");
-// }
-// }
+do {
+    var custCones = Math.floor(Math.random() * 5) + 1;
+    // console.log(cones + " is how many cones we need to sell.");
+    // console.log(custCones + " is how many they want to buy right now.");
 
+    // if (cones < 0) {
+    //     console.log("You can't sell that much ice cream because you don't have enough in stock, find a new customer.");
+    // } else if (cones === 0) {
+//     console.log("You sold every single cone!")
+// // }
+    if (allCones >= custCones) {
+        console.log("You sold " + custCones + " cones.")
+        allCones =allCones - custCones;
+        console.log(allCones + " is how many are left.")
 
- // var cones = Math.floor(Math.random() * 50) + 50;
- // do {
- //     var custCones = Math.floor(Math.random() * 5) + 1;
- //     var afterSale = cones-=custCones
- //     console.log(cones + " is how many cones we need to sell.");
- //     console.log(afterSale + " is how many cones are left now.");
- //     console.log(custCones + " is how many cones they want to buy right now.");
- //     afterSale;
- //    }
- // while(afterSale - custCones >= 0) {
- //
- //
- //
- // }
+    } else {
+        console.log("You cant sell that many we have "+ allCones + " they want " + custCones)
+    }
+
+} while (allCones >= 1);
+
+console.log("You have sold all the cones.")
 
 
-//
+
+
 
 
 
