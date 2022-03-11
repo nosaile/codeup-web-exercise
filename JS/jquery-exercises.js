a768b8e72534e315cf6379fb50ec8367d4aa553d
Chris Eliason <eliason.chris1@gmail.com>
2022-03-11 12:04:15 -0600

    finished selectors

--- JS/jquery-exercises.js
+++ JS/jquery-exercises.js
@@ -3,3 +3,26 @@
 $(document).ready(function (){
     alert("the dom is done loading")
 });
+
+
+const name = $("#name").html();
+alert(name);
+
+
+$('.codeup').css('border', '1px solid red');
+
+$('#mtn, #fun-time').css('font-size', '20px');
+
+
+
+$('h1, p, li').css('background', 'yellow');
+
+
+
+
+let text ;
+    $(document).ready(function (){
+         text = $('h1').text();
+        alert(text)
+    });
+
