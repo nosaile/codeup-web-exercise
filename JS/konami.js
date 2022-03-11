2e00652970989920688c71d1549904606b9caac9
Chris Eliason <eliason.chris1@gmail.com>
2022-03-11 16:22:36 -0600

    konami

--- JS/konami.js
+++ JS/konami.js
@@ -1,6 +1,51 @@
 "use strict";
 
+//
+// $(document).ready(function (){
+//     let konamiCode = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65, 13];
+//     let codePosition = 0;
+//
+//
+//
+//     $(document).keyup(function(e){
+//         console.log(e.keyCode);
+//
+//         let expectedPosition = konamiCode[codePosition];
+//         let keyPress = e.keyCode;
+//
+//
+//
+//
+//
+//
+//     });
+//
+//
+// })
+function konami(fn) {
+    var input = "";
+    var pattern = "3838404037393739666513";
+    $(document).keydown(function(e) {
+        input += e.keyCode;
+        if (input.indexOf(pattern) !== -1) {
+            fn();
+            input = "";
 
-$(document).ready(function (){
+        }
+    });
+}
+let audio = $('audio')[0]
 
-})
+$(document).ready(function() {
+    konami(function() {
+        alert("kool-nami");
+        $("body").css("background-image", "url(../images/koolaid.gif)");
+        audio.play();
+        $('.flasher');
+
+
+    });
+});
+
+
+
