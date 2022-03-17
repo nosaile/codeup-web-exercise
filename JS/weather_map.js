7a8143801ec7748d35901ad720ca51bf6cd47fb1
Chris Eliason <eliason.chris1@gmail.com>
2022-03-17 14:04:53 -0500

    search feature, not tied to weather data but on the page

--- JS/weather_map.js
+++ JS/weather_map.js
@@ -1,16 +1,25 @@
 "use strict";
-
 //weather box data
-// var lat = $('#searchlat').val()
-// var lon = $('#searchlon').val()
 
 
-var lat = '32.75';
-var lon = '-97';
-fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=${OWM_KEY}`)
+
+// var lat =
+// var lon =
+
+
+
+
+
+var lat = 32.75;
+var lon = -97;
+
+newWeather(lat, lon)
+function newWeather(lat, lon){
+
+
+    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=${OWM_KEY}`)
     .then(response => response.json())
     .then(data => {
-        console.log(data)
         let html = '';
         for (let i = 0; i < 5; i++) {
             html += '<div class="card col-2 bg-secondary text-light text-center" style="width: 11%; border-color: #EB9D3A; border-style: solid">'
@@ -19,41 +28,51 @@ fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&uni
             let dailyFeelsNight = data.daily[i].feels_like.night;
             let uvIndex = data.daily[i].uvi;
             let windSpeed = data.daily[i].wind_speed;
+            var myDate = new Date( data.daily[i].dt *1000).toLocaleString();
             html += '<div class="card-header bg-dark text-light" style="height: 2em">'
-            html += '<p> Day ' + [i + 1] + '</p>';
+            html += '<p>'+myDate.substring(0, 9)+ '</p>';
             html += '</div>'
-            html += '<p>Temp ' + Math.floor(dailyTemp) + ' º</p>' + '<p> Feels like: ' + Math.floor(dailyFeelsDay) + 'º daytime.</p>' + '<p> Feels like: ' + Math.floor(dailyFeelsNight) + 'º at night.</p>' + '<p> UV Index: ' + uvIndex + ' out of 10</p>' + '<p> Wind Speed: ' + windSpeed + ' mph</p>'
+            html += '<p>Temp: ' + Math.floor(dailyTemp) + 'º</p>' + '<p> Feels like: ' + Math.floor(dailyFeelsDay) + 'º daytime.</p>' + '<p> Feels like: ' + Math.floor(dailyFeelsNight) + 'º at night.</p>' + '<p> UV Index: ' + uvIndex + ' out of 10</p>' + '<p> Wind Speed: ' + windSpeed + ' mph</p>'
             html += '</div>'
-
         }
         html += '<div class="form-control bg-secondary row" style="width: 25%; border-color: #EB9D3A; border-style: solid" id="searchers">' +
             '<div class="col-3"><input id="searchlat" placeholder="latitude"></div>' +
             '<div class="col-3"><input id="searchlon" placeholder="longitude"></div>' +
             '<button class="col-4" id="search-submit">submit</button>' +
             '</div>'
-
-
         $('#weather').html(html)
         $('#search-submit').click(function () {
             lat = $('#searchlat').val();
             lon = $('#searchlon').val();
-            console.log(lat, lon)
+            newWeather(lat, lon)
             mapboxgl.accessToken = MBX_KEY;
             var map = new mapboxgl.Map({
                 container: 'map', style: 'mapbox://styles/mapbox/streets-v9', zoom: 8, center: [lon, lat]
             });
         });
-
     });
-
+}
 //map box
 mapboxgl.accessToken = MBX_KEY;
 var map = new mapboxgl.Map({
     container: 'map', style: 'mapbox://styles/mapbox/streets-v9', zoom: 10, center: [lon, lat]
+
+
 });
+//mapbox geo coder
+
+map.addControl(
+    new MapboxGeocoder({
+        accessToken: mapboxgl.accessToken,
+        mapboxgl: mapboxgl
+    })
+);
 
 
-//Weather Map head
+
+//
+//
+//
 
 
 
