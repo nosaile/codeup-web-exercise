4fae2c8c6d10b6d45b3bc1150075881fe458a349
Chris Eliason <eliason.chris1@gmail.com>
2022-03-18 12:20:42 -0500

    extra descriptions to restraunts

--- JS/mapbox_maps_api.js
+++ JS/mapbox_maps_api.js
@@ -1,5 +1,5 @@
 "use strict";
-let zoom = 15
+let zoom = 10
 
 
 mapboxgl.accessToken = MBX_KEY;
@@ -13,22 +13,21 @@ var map = new mapboxgl.Map({
 
 });
 
-
 function restrauntInfo() {
     return [{
         name: "Cane Corso",
         location: [-97.3332, 32.7305],
-        description: "pizza"
+        description: "good pizza, lots of good drinks"
     },
         {
             name: "Rio Mambo",
             location: [-97.416470, 32.680890],
-            description: "tex-mex"
+            description: "tex-mex, rarely busy, great salsa"
         },
         {
             name: "Crazy Tomato",
             location: [-82.374450, 36.346960],
-            description: "italian"
+            description: "italian, my favorite as a kid, waiter that works there named \"baby face\""
         }
 
     ]
@@ -38,14 +37,9 @@ restrauntInfo().forEach(myFunction);
 
 function myFunction(restaurant) {
     markerPopup()
-    console.log(restaurant)
-
-
 }
 
 //markers
-
-
 function markerPopup() {
     for (let i = 0; i < restrauntInfo().length; i++) {
 
@@ -55,9 +49,7 @@ function markerPopup() {
         })
             .setLngLat([restraunt[i].location[0], restraunt[i].location[1]])
             .addTo(map)
-
 //popups
-
         var popup = new mapboxgl.Popup()
             .setHTML("<p>" + restrauntInfo()[i].name + "</p>" +
                 "<p>" + restrauntInfo()[i].description + "</p>")
@@ -66,6 +58,5 @@ function markerPopup() {
 }
 
 let html = '';
-
 
 $('#footer').html(html)
