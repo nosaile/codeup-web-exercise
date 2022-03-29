7f4c24e126674a002a8d4efde8f8f9b817d09531
Chris Eliason <98778355+nosaile@users.noreply.github.com>
2022-03-29 12:30:50 -0500

    promises ex

--- JS/promises.js
+++ JS/promises.js
@@ -1,19 +1,5 @@
 "use strict";
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // renderGitData()
 // function renderGitData() {
 //     fetch(`https://api.github.com/users/nosaile/events`, {headers: {'Authorization': `${GIT_KEY}`}})
@@ -24,21 +10,27 @@
 // };
 //
 // function findGitData(data){
-//     console.log(data)
-//     console.log(data[0].created_at)
-//
+//   var latestCommit = data[0].commit.author.date.substring(0, 10);
+//     console.log(latestCommit)
+//     let html = '';
+//     html += '<h1>the github user \'nosaile\' last made a commit on: <br>' + latestCommit + '</h1>'
+//     $('.content').html(html)
 // }
+
 renderGitData()
+
 function renderGitData() {
 
-fetch("https://api.github.com/repos/nosaile/codeup-web-exercises/commits", {headers: {'Authorization': `${GIT_KEY}`}})
-    .then(response => response.json())
+    fetch("https://api.github.com/repos/nosaile/codeup-web-exercises/commits", {headers: {'Authorization': `${GIT_KEY}`}})
+        .then(response => response.json())
         .then(data => findGitData(data))
         .catch(error => console.log(error))
 }
-function findGitData(data){
-    console.log(data)
-   let latestCommit =  data[0].commit.author.date.substring(0,10)
-    console.log(latestCommit)
 
+function findGitData(data) {
+    var latestCommit = data[0].commit.author.date.substring(0, 10);
+    console.log(latestCommit)
+    let html = '';
+    html += '<h1>the github user \'nosaile\' last made a commit on: <br>' + latestCommit + '</h1>'
+    $('.content').html(html)
 }
